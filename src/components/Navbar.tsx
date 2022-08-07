ef9a4c88738bd3da409eed568f5357f75d0f5d8a
알파 <arpa1705@gmail.com>
2022-08-07 16:44:03 +0900

    feat: 소개 문구 추가 및 영상 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,8 @@
-import Link from 'next/link';
-import Router from 'next/router';
-import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { useEffect } from 'react';
 import { animateScroll } from 'react-scroll';
 
 const Navbar: React.FC = () => {
-  const [temp, setTemp] = useState(0);
-
   useEffect(() => {
     const handleResize = () => {
       window.scrollTo({
@@ -21,7 +18,7 @@ const Navbar: React.FC = () => {
   }, []);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 px-5 py-4 flex bg-transparent backdrop-blur-xl z-[99999] transition-all duration-300">
+    <nav className="fixed top-0 left-0 right-0 px-5 py-3 flex items-center bg-transparent backdrop-blur-xl z-[99999] transition-all duration-300">
       <a
         className="font-extrabold text-3xl cursor-pointer px-2 py-1"
         onClick={() => {
@@ -29,6 +26,16 @@ const Navbar: React.FC = () => {
         }}
       >
         4096
+      </a>
+      <a
+        className="ml-auto flex items-center gap-3 text-lg font-semibold px-4 py-2 hover:bg-white/5 transition-all duration-300 rounded-2xl"
+        href="https://inft.kr"
+        target="_black"
+        rel="norefferrer"
+      >
+        <span className="font-light">By</span>
+        <Image src="/infiniteteam.png" priority width={20} height={20} />
+        <span className="text-xl">InfiniteTeam</span>
       </a>
     </nav>
   );
